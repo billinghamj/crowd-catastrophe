7b89fa291c776ffd05c8646c39be32c03b32b4fb
James Billingham <james@billingham.net>
2014-09-14 00:10:59 +0100

    Method override

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var expressHandlebars = require('express-handlebars');
 var http = require('http');
 var path = require('path');
@@ -31,6 +32,14 @@ app.set('models', db(app));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(methodOverride(function (req, res) {
+	if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+		var method = req.body._method;
+		delete req.body._method;
+		return method;
+	}
+}));
 
 routes(app);
 
