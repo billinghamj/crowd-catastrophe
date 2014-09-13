2029085e088086ff859162744fc4ca4f3cff8736
Will Evans <will@will3942.com>
2014-09-13 17:56:55 +0100

    relation

--- app/db/index.js
+++ app/db/index.js
@@ -16,9 +16,13 @@ function setup(app) {
 	var modelsObject = models(sequelize);
 
 	modelsObject.Issue.hasMany(modelsObject.Tag, {through: 'issue_tags'});
+	modelsObject.Tag.belongsTo(modelsObject.Issue, {through: 'issue_tags'});
 	modelsObject.Media.hasMany(modelsObject.Tag, {through: 'tag_media'});
+	modelsObject.Tag.belongsTo(modelsObject.Media, {through: 'tag_media'});
 	modelsObject.Tag.hasMany(modelsObject.Media, {through: 'tag_media'});
+	modelsObject.Media.belongsTo(modelsObject.Tag, {through: 'tag_media'});
 	modelsObject.Tag.hasMany(modelsObject.Issue, {through: 'issue_tags'});
+	modelsObject.Issue.belongsTo(modelsObject.Tag, {through: 'issue_tags'});
 
 	sequelize.sync();
 	return modelsObject;
