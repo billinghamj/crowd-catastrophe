ec4685337a079a394997ae958a17e76ed8d17fb9
James Billingham <james@billingham.net>
2014-09-13 16:35:49 +0100

    v cleanup

--- migrations/20140913140955-create_tables.js
+++ migrations/20140913140955-create_tables.js
@@ -1,64 +1,47 @@
 module.exports = {
-  up: function(migration, DataTypes, done) {
+	up: function (migration, DataTypes, done) {
 		migration.createTable(
-		  'Media',
-		  {
+			'media',
+			{
 				id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
 				instagramId: { type: DataTypes.STRING, allowNull: false, unique: true },
 				date: { type: DataTypes.DATE, allowNull: true },
 				thumbnailUrl: { type: DataTypes.STRING, allowNull: false },
 				imageUrl: { type: DataTypes.STRING, allowNull: false },
-				createdAt: {
-		      type: DataTypes.DATE
-		    },
-		    updatedAt: {
-		      type: DataTypes.DATE
-		    },
-		  },
-		  {
-		    engine: 'InnoDB', // default: 'InnoDB'
-		    charset: 'latin1' // default: null
-		  }
-		)
+				createdAt: { type: DataTypes.DATE },
+				updatedAt: { type: DataTypes.DATE }
+			},
+			{ engine: 'InnoDB', charset: 'latin1' }
+		);
+
 		migration.createTable(
-		  'Issue',
-		  {
+			'issues',
+			{
 				id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
 				name: { type: DataTypes.STRING, allowNull: false, unique: true  },
 				description: { type: DataTypes.STRING, allowNull: true },
 				date: { type: DataTypes.DATE, allowNull: true },
-				createdAt: {
-		      type: DataTypes.DATE
-		    },
-		    updatedAt: {
-		      type: DataTypes.DATE
-		    },
-		  },
-		  {
-		    engine: 'InnoDB', // default: 'InnoDB'
-		    charset: 'latin1' // default: null
-		  }
-		)
-		migration.createTable(
-		  'Tag',
-		  {
-				name: { type: DataTypes.STRING, allowNull: false, primaryKey: true, unique: true },
-				createdAt: {
-		      type: DataTypes.DATE
-		    },
-		    updatedAt: {
-		      type: DataTypes.DATE
-		    },
+				createdAt: { type: DataTypes.DATE },
+				updatedAt: { type: DataTypes.DATE }
 			},
-		  {
-		    engine: 'InnoDB', // default: 'InnoDB'
-		    charset: 'latin1' // default: null
-		  }
-		)
-    done()
-  },
-  down: function(migration, DataTypes, done) {
-		migration.dropAllTables()
-    done()
-  }
-}
+			{ engine: 'InnoDB', charset: 'latin1' }
+		);
+
+		migration.createTable(
+			'tags',
+			{
+				name: { type: DataTypes.STRING, allowNull: false, primaryKey: true, unique: true },
+				createdAt: { type: DataTypes.DATE },
+				updatedAt: { type: DataTypes.DATE }
+			},
+			{ engine: 'InnoDB', charset: 'latin1' }
+		);
+
+		done();
+	},
+
+	down: function (migration, DataTypes, done) {
+		migration.dropAllTables();
+		done();
+	}
+};
