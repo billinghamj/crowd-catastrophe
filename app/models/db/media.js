614acef454bb24940bf7cbcf33b7891d9db22191
James Billingham <james@billingham.net>
2014-09-13 13:28:51 +0100

    very image urls

--- app/models/db/media.js
+++ app/models/db/media.js
@@ -6,7 +6,9 @@ function setup(sequelize) {
 	var Media = sequelize.define('Media', {
 		id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
 		instagramId: { type: Sequelize.STRING, allowNull: false },
-		date: { type: Sequelize.DATE, allowNull: true }
+		date: { type: Sequelize.DATE, allowNull: true },
+		thumbnailUrl: { type: Sequelize.STRING, allowNull: false },
+		imageUrl: { type: Sequelize.STRING, allowNull: false }
 	});
 
 	return Media;
