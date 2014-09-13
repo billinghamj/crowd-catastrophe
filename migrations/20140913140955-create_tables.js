3c766b2ad5555d31a12233af054496a8fe45a8c0
Will Evans <will@will3942.com>
2014-09-13 14:16:07 +0100

    troye sivan

---
+++ migrations/20140913140955-create_tables.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: function(migration, DataTypes, done) {
+		migration.createTable(
+		  'Media',
+		  {
+				id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+				instagramId: { type: DataTypes.STRING, allowNull: false },
+				date: { type: DataTypes.DATE, allowNull: true },
+				thumbnailUrl: { type: DataTypes.STRING, allowNull: false },
+				imageUrl: { type: DataTypes.STRING, allowNull: false }
+		  },
+		  {
+		    engine: 'InnoDB', // default: 'InnoDB'
+		    charset: 'null' // default: null
+		  }
+		)
+		migration.createTable(
+		  'Issue',
+		  {
+				id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+				name: { type: DataTypes.STRING, allowNull: false },
+				description: { type: DataTypes.STRING, allowNull: true },
+				date: { type: DataTypes.DATE, allowNull: true }
+		  },
+		  {
+		    engine: 'InnoDB', // default: 'InnoDB'
+		    charset: 'null' // default: null
+		  }
+		)
+		migration.createTable(
+		  'Tag',
+		  {
+				name: { type: DataTypes.STRING, allowNull: false, primaryKey: true }
+			},
+		  {
+		    engine: 'InnoDB', // default: 'InnoDB'
+		    charset: 'null' // default: null
+		  }
+		)
+    done()
+  },
+  down: function(migration, DataTypes, done) {
+		migration.dropAllTables()
+    done()
+  }
+}
