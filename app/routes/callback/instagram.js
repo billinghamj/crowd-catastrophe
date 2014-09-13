5e26b18746b966f88e903b2c563677b8d4e4972a
James Billingham <james@billingham.net>
2014-09-13 21:44:49 +0100

    Unneeded logs

--- app/routes/callback/instagram.js
+++ app/routes/callback/instagram.js
@@ -62,7 +62,6 @@ function ingest(req, res, next) {
 				models.Tag.findAll().success(function (tags) {
 					// remove tags we already have
 					for (var i = 0; i < tags.length; i++) {
-						console.log(tags[i].name);
 						var j = tagsNeeded.indexOf(tags[i].name);
 						if (j !== -1) tagsNeeded[j] = null;
 					}
