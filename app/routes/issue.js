4c7623566a29891d2c437c5e5547e9700244b850
James Billingham <james@billingham.net>
2014-09-14 15:24:09 +0100

    no message

--- app/routes/issue.js
+++ app/routes/issue.js
@@ -68,7 +68,8 @@ function show(req, res, next) {
 				+ ' JOIN issue_tags it ON it.tagId = tm.tagId'
 				+ ' JOIN issues i ON i.id = it.issueId'
 				+ ' WHERE i.id = ' + issue.id
-				+ ' ORDER BY m.id DESC';
+				+ ' ORDER BY RAND()'
+				+ ' LIMIT 500';
 
 			req.app.get('models')
 				._sequelize.query(sql, req.app.get('models').Media)
